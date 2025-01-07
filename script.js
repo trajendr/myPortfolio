318cf1a58bccc7ad6a13ce182c88862ecb15011d
trajendr <thanur655@gmail.com>
2025-01-07 20:24:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for your message! I will get back to you soon.');
+});
